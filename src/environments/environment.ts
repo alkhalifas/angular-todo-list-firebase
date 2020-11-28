f243662bfdf0140e8594b240246f6ffdb81d15f7
Saleh Alkhalifa <salehesam@gmail.com>
2020-11-28 18:46:30 -0500

    config setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,17 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyDyql40_MXE4-p6mY-SfMvKL9_icFjfDeA',
+  authDomain: 'stox-c6eb9-v1.firebaseapp.com',
+  databaseURL: 'https://stox-c6eb9-v1.firebaseio.com',
+  projectId: 'stox-c6eb9-v1',
+  storageBucket: 'stox-c6eb9-v1.appspot.com',
+  messagingSenderId: '311361994687',
+  appId: '1:311361994687:web:38956e11743b029961eb0a',
+  measurementId: 'G-VHJ4EYX2T7'
+};
+
 export const environment = {
   production: false
 };
